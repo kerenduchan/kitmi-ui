71c65d0b7c0d023a5aab20cc634eb63573632255
Keren Duchan <keren.duchan@gmail.com>
2022-12-10 22:27:58 +0200

    wip: edit mode for subcategory

--- src/components/Subcategory.vue
+++ src/components/Subcategory.vue
@@ -16,11 +16,48 @@ const emit = defineEmits([
     'subcategoryEdited',
 ])
 
+const isEditMode = ref(false)
+
+const newName = ref(props.subcategory.name)
+
+const newNameInput = ref(null)
+
+
+function deleteSubcategory() {
+    console.log('delete')
+}
+
+function renameSubcategory() {
+    console.log('renameSubcategory to ' + newName.value)
+}
+
+function enterEditMode() {
+    isEditMode.value = true
+    newName.value = props.subcategory.name
+}
+
+function exitEditMode() {
+    isEditMode.value = false
+}
+
+
+
 </script>
 
 <template>
 
-<div>({{ props.subcategory.id }}) {{ props.subcategory.name }}</div>
+<div>
+    <span>({{ props.subcategory.id }}) </span>
+    <span v-if="!isEditMode">
+        <span  @click="enterEditMode">{{ props.subcategory.name }}</span>   
+        <span @click="deleteSubcategory"> - </span>
+    </span>
+    <span v-else>
+            <input type="text" ref="newNameInput" @keyup.escape="exitEditMode" v-model="newName" />
+            <button @click="renameSubcategory">Save</button>
+            <button @click="exitEditMode">Cancel</button>
+    </span>
+</div>
 
 </template>
 

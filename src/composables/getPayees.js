16b22aa90996e389441ccf41ab4734c43b17dd51
Keren Duchan <keren.duchan@gmail.com>
2022-12-11 17:56:37 +0200

    getPayees composable

---
+++ src/composables/getPayees.js
@@ -0,0 +1,35 @@
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { ref } from 'vue'
+
+function getPayees() {
+    const { onResult, refetch } = useQuery(gql`
+        query getPayees {
+            payees {
+                id
+                name
+                subcategory {
+                    id
+                    name
+                    category {
+                        id
+                        name
+                        isExpense
+                    }
+                }
+            }
+        }
+    `)
+
+    const payees = ref(null)
+    const isReady = ref(false)
+
+    onResult(queryResult => {
+        payees.value = queryResult.data.payees
+        isReady.value = true
+    })
+
+    return { payees, isReady, refetch }
+}
+
+export default getPayees
--- src/views/PayeesView.vue
+++ src/views/PayeesView.vue
@@ -1,35 +1,8 @@
 <script setup>
-import { useQuery } from '@vue/apollo-composable'
-import gql from 'graphql-tag'
-import { ref } from 'vue'
 import PayeesList from '../components/PayeesList.vue'
+import getPayees from '../composables/getPayees'
 
-const { onResult, refetch } = useQuery(gql`
-        query getPayees {
-            payees {
-                id
-                name
-                subcategory {
-                    id
-                    name
-                    category {
-                        id
-                        name
-                        isExpense
-                    }
-                }
-            }
-        }
-    `)
-
-const isReady = ref(false)
-
-const payees = ref(null)
-
-onResult(queryResult => {
-  payees.value = queryResult.data.payees
-  isReady.value = true
-})
+const { payees, isReady, refetch } = getPayees()
 
 function onChange() {
     console.log('A change ocurred. Refetching.')

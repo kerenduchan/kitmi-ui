1276bc753e4d25018245ea5a02c6aee6715fe6e2
Keren Duchan <keren.duchan@gmail.com>
2022-12-17 14:27:06 +0200

    category actions wip

--- src/components/CategoriesList.vue
+++ src/components/CategoriesList.vue
@@ -6,16 +6,23 @@ const props = defineProps({
     categories: Object
 })
 
-const headers = ref(['Type', 'Name'])
+// emits
+const emit = defineEmits([
+    'selectedItemChanged'
+])
+
+function handleCategoryClicked(category) {
+    emit('selectedItemChanged', category)
+}
 
 </script>
 
 <template>
     <h2>Categories</h2>
 
-    <v-expansion-panels multiple>
+    <v-expansion-panels>
         <v-expansion-panel v-for="c in props.categories" :key="c.id">
-            <v-expansion-panel-title> {{ c.name }} </v-expansion-panel-title>
+            <v-expansion-panel-title @click="handleCategoryClicked(c)"> {{ c.name }} </v-expansion-panel-title>
             <v-expansion-panel-text>
                 <v-list>
                     <v-list-item v-for="s in c.subcategories" :key="s.id" :title="s.name" />
--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,20 +1,74 @@
 <script setup>
+import { ref } from 'vue'
 import CategoriesList from '@/components/CategoriesList.vue'
 import getCategories from '@/composables/queries/getCategories'
 
 const { categories, isReady, refetch } = getCategories()
 
+const selectedItem = ref(null)
+
+function handleSelectedItemChanged(item) {
+    console.log('selected item changed ' + item.name)
+    selectedItem.value = item
+}
+
+function handleEditClicked() {
+    console.log('handleEditClicked')
+}
+
+function handleCreateCategoryClicked() {
+    console.log('handleCreateCategoryClicked')
+}
+
+function handleDeleteClicked() {
+    console.log('handleDeleteClicked')
+}
+
+function handleCreateSubcategoryClicked() {
+    console.log('handleCreateSubcategoryClicked')
+}
+
 </script>
-
-
 
 <template>
     <div class="top-bar">
+        <div class="top-bar-left">
+            <div class="top-bar-action">
+                <!-- Edit button -->
+                <v-btn 
+                    icon="mdi-pencil" 
+                    @click="handleEditClicked">
+                </v-btn>
+            </div>
+            <div class="top-bar-action">
+                <!-- Add subcategory button -->
+                <v-btn 
+                    icon="mdi-plus" 
+                    @click="handleCreateSubcategoryClicked">
+                </v-btn>
+            </div>
+            <div class="top-bar-action">
+                <!-- Delete button -->
+                <v-btn 
+                    icon="mdi-delete" 
+                    @click="handleDeleteClicked">
+                </v-btn>
+            </div>
+        </div>
+        <div class="top-bar-right">
+            <div class="top-bar-action">
+                <!-- Create category button -->
+                <v-btn 
+                    icon="mdi-plus" 
+                    @click="handleCreateCategoryClicked">
+                </v-btn>
+            </div>
+        </div>
     </div>
     <v-divider />
 
     <div v-if="!isReady">Loading...</div>
     <div v-else>
-        <CategoriesList :categories="categories" />
+        <CategoriesList :categories="categories" @selectedItemChanged="handleSelectedItemChanged"/>
     </div>
 </template>

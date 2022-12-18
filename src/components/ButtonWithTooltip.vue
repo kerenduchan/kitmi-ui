d11b9f2f80031362515b54dfb430c5865b95d396
Keren Duchan <keren.duchan@gmail.com>
2022-12-18 18:41:28 +0200

    disable tooltip on disabled button

--- src/components/ButtonWithTooltip.vue
+++ src/components/ButtonWithTooltip.vue
@@ -10,7 +10,7 @@ const emit = defineEmits(['click'])
 </script>
 
 <template>
-    <v-tooltip :text="tooltip" location="bottom">
+    <v-tooltip :text="tooltip" :disabled="disabled" location="bottom">
         <template v-slot:activator="{ props }">
             <v-btn class="rounded-0" elevation="0" v-bind="props" :icon="icon" :disabled="disabled" @click="emit('click')">
             </v-btn>

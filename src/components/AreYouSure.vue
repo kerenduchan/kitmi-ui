cea85111fd5b3fe0eed2e5c8fa8f66143d714294
Keren Duchan <keren.duchan@gmail.com>
2022-12-23 16:21:55 +0200

    new AreYouSure component

---
+++ src/components/AreYouSure.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+// props 
+const props = defineProps({
+    title: String
+})
+
+const emit = defineEmits([
+    'cancel',
+    'yes'
+])
+
+</script>
+
+<template>
+    <v-card>
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-text>
+            Are you sure?
+        </v-card-text>
+        <v-card-actions>
+            <v-btn color="primary" @click="emit('yes')">Yes</v-btn>
+            <v-btn color="primary" @click="emit('cancel')">Cancel</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>

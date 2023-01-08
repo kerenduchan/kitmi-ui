ab43a0a0500450bec6fac7d68515ebf26199a96d
Keren Duchan <keren.duchan@gmail.com>
2023-01-08 13:30:53 +0200

    style summary table

--- src/components/SummaryTable.vue
+++ src/components/SummaryTable.vue
@@ -23,24 +23,40 @@ const headers = computed(() => {
     <v-table density="compact">
         <thead>
             <tr>
-                <th v-for="header in headers" class="text-left">
+                <th v-for="header in headers" class="text-center table-header-cell">
                     {{ header }}
                 </th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="g in props.summary.groups">
-                <td>{{ g.name }}</td>
+                <td class="table-header-cell">{{ g.name }}</td>
                 <td v-for="v in g.data " class="text-right">{{ formatNumber(v, 0) }}</td>
-                <td class="text-right">{{ formatNumber(g.total, 0) }}</td>
+                <td class="text-right font-weight-bold">{{ formatNumber(g.total, 0) }}</td>
             </tr>
         </tbody>
         <tfoot>
             <tr>
-                <td>Total</td>
-                <td v-for="v in props.summary.totals" class="text-right">{{ formatNumber(v, 0) }}</td>
+                <td class="table-header-cell font-weight-bold">Total</td>
+                <td v-for="v in props.summary.totals" class="text-right font-weight-bold">{{ formatNumber(v, 0) }}</td>
             </tr>
         </tfoot>
     </v-table>
 
 </template>
+
+<style>
+    .table-header-cell {
+        background-color: lightgray;
+        font-weight: bold;
+    }
+
+    .table-summary-cell {
+        background-color: #FFF9E8;
+        font-weight: bold;
+    }
+
+    table, th, td {
+  border: 0.5px solid black;
+}
+</style>

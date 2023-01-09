0632037d9c467ef3978954fa37af61e0e80e27ef
Keren Duchan <keren.duchan@gmail.com>
2023-01-09 15:09:19 +0200

    remove log print

--- src/composables/utils.js
+++ src/composables/utils.js
@@ -33,9 +33,7 @@ function getChartColors() {
 }
 
 function isReservedName(name) {
-    const b = ['total', 'other'].includes(name.toLowerCase())
-    console.log('isReservedName ' + name + ' - ' + b)
-    return b
+    return ['total', 'other'].includes(name.toLowerCase())
 }
 
 export {

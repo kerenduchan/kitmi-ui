af2e96c67dc09b3b53b772918143d2ecc3d1b4f8
Keren Duchan <keren.duchan@gmail.com>
2023-02-04 10:11:07 +0200

    handle case when there are no uncategorized payees

--- src/components/payeesCategorizationWizard/WizardLoader.vue
+++ src/components/payeesCategorizationWizard/WizardLoader.vue
@@ -25,16 +25,42 @@ const { onResult } = getPayees({
     categorized: false
 })
 
+const showNoUncategorizedPayeesDialog = ref(false)
+
 onResult(res => {
     payees.value = res.data.payees.items.map((p) => Payee.fromGql(p))
+    if(payees.value.length === 0) {
+        showNoUncategorizedPayeesDialog.value = true
+    }
 })
 
 function handleSave(payees) {
     emit('save', payees)
 }
 
+function cancel() {
+    emit('cancel')
+}
+
+function handleNoUncategorizedPayeesDialogOk() {
+    showNoUncategorizedPayeesDialog.value = false
+    cancel()
+}
+
 </script>
 
 <template>
-<Wizard v-if="payees" :payees="payees" :categories="categories" @save="handleSave" @cancel="emit('cancel')"/>
+    <template v-if="payees">
+        <v-dialog v-model="showNoUncategorizedPayeesDialog" width="400px">
+            <v-card title="Categorization Wizard" text="There are no uncategorized payees.">
+                <v-card-actions>
+                    <v-btn @click="handleNoUncategorizedPayeesDialogOk">OK</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <Wizard v-if="payees.length > 0" :payees="payees" :categories="categories" @save="handleSave" @cancel="cancel"/>
+        
+    </template>
+
+    
 </template>

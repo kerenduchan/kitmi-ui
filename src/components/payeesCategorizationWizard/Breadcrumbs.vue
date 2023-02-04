7026b2a6369b957c8e546bb987b8e91efe7c385f
Keren Duchan <keren.duchan@gmail.com>
2023-02-04 09:43:14 +0200

    breadcrumbs styling

--- src/components/payeesCategorizationWizard/Breadcrumbs.vue
+++ src/components/payeesCategorizationWizard/Breadcrumbs.vue
@@ -12,18 +12,29 @@ const emit = defineEmits([
 </script>
 
 <template>
-    <div class="pt-3">
+    <div class="pt-3 d-flex gap">
         Categorization:
-        <span v-if="category === null">
+        <span v-if="!category">
             <span class="text-excluded font-italic">Uncategorized</span>
         </span>
         <span v-else>
             <a @click="emit('categoryClicked')" class="text-link font-weight-bold">{{ category.name }}</a>
         </span>
-        <span v-if="subcategory !== null">
+        <span v-if="category">
             >
+        </span>
+        <span v-if="subcategory">
             <a @click="emit('subcategoryClicked')" class="text-link font-weight-bold">{{ subcategory.name }}</a>
+        </span>
+        <span v-if="category && !subcategory">
+            <span class="text-excluded font-italic">Uncategorized</span>
         </span>
     </div>
 
 </template>
+
+<style scoped>
+.gap {
+    gap: 10px;
+}
+</style>
